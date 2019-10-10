63fbb6d5b97366ae49443d227e5b670bfac13063
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2019-10-10 06:41:11 +0530

    morning challenge finishded

--- bin/main.dart
+++ bin/main.dart
@@ -69,15 +69,74 @@
 // Challenge 2
 // Validate the move
 // In case if there is already an entry on board print invalid move
-
+import 'dart:io';
+bool firstrun=true;
+int i=0;
 main() {
   var boardSize = 3;
   List<List<String>> board =
   List.generate(boardSize, (_) => List.filled(boardSize, ' '));
+  printBoard(board);
+  String currentPlayer='X';
+  List<int>x=[];
+
+  List<int>y=[];
+  int tempx, tempy;
+
+  while(true)
+  {
+    print('$currentPlayer\'s move');
+    String userChoice=stdin.readLineSync();
+    tempx=getRow(userChoice);
+    tempy=getColumn(userChoice);
+    if(validateUserInput(x,  y,tempx, tempy))
+      {
+        x.add(tempx);
+        y.add(tempy);
+        print("Printing List x");
+        print(x);
+        print("Printing List y");
+        print(y);
+        board[x[i]][y[i]]=currentPlayer;
+        printBoard(board);
+        i++;
+        if(currentPlayer=='X')
+        {
+          currentPlayer='O';
+        }
+        else
+        {
+          currentPlayer='X';
+        }
+      }
+
+
+  }
+
 
 
 }
+bool validateUserInput(List<int>x,List<int>y,int tempx, int tempy)
+{
+  if(firstrun)
+  {
+    print("first run");
+    firstrun=false;
+    return true;
+  }
+  for(int j=0;j<x.length;j++)
+    {
 
+      if((x[j]==tempx)&&(y[j]==tempy))
+        {
+          print("Invalid Input. Try Again!!");
+          return false;
+        }
+
+
+    }
+  return true;
+}
 void printBoard(List<List<String>> board) {
   print('   A   B   C ');
   List<String> rows = board.map(rowToString).toList();
@@ -88,6 +147,27 @@ void printBoard(List<List<String>> board) {
   print('3 ${rows[2]}');
 }
 
+int getColumn(String userInput){
+  if(userInput[0]=='A') {
+    return 0;
+  }else if(userInput[0]=='B') {
+    return 1;
+  }else{
+    return 2;
+  }
+}
+
+int getRow(String userInput){
+  if(userInput[1]=='1') {
+    return 0;
+  }else if(userInput[1]=='2') {
+    return 1;
+  }else{
+    return 2;
+  }
+}
+
 String rowToString(List<String> row) {
   return row.map((String val) => ' $val ').join('|');
 }
+
